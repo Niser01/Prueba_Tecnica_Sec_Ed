c3411809f7995e461f3c3f5c6f0a9cdcd4a50adc
Sergio Nicolás <64512516+Niser01@users.noreply.github.com>
2025-09-05 10:25:20 -0500

    Spring Boot creado

---
+++ backend/src/test/java/com/prueba_tecnica_sec_ed/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba_tecnica_sec_ed.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
